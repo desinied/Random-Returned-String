053b38a4d1c3a948dfdb19414c6e68d7bc71591d
William | desinied <120297669+desinied@users.noreply.github.com>
2023-01-23 23:09:34 -0500

    Added PathParser & Length checking

--- DeepDictionarySearch/main.lua
+++ DeepDictionarySearch/main.lua
@@ -1,7 +1,11 @@
-function DeepDictionarySearch(RootDictionary: table, Path: string)
-    local PathKeys = string.split(Path, ".")
+function DeepDictionarySearch(RootDictionary: table, Path: string, PathParser: string)
+    if string.len(PathParser) ~= 1 then 
+		error("DeepDictionarySearch has been given too manny/little PathParser characters as a paramater!\nPlease only use a 1 in length character PathParcer!") 
+		return nil 
+	end
 	
-	if #PathKey == 0 then print("DeepDictionarySearch has failed, there were no path keys to parse!") return nil end
+	local PathKeys = string.split(Path, PathParser)
+	if #PathKey == 0 then warn("DeepDictionarySearch has failed, there were no path keys to parse!") return nil end
 	
 	local CurrentKey = RootDictionary
 	for i = 1, #PathKeys do
